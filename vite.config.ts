ed5d3dc71340c2fa44802c4365ccd969a993571d
feelslikemmmm@gmail.com <67893516+feelslikemmmm@users.noreply.github.com>
2025-01-01 23:15:25 +0900

    chore(low): 절대경로 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import withReactRouter from 'vite-plugin-next-react-router';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    withReactRouter({
+      pageDir: 'src/pages',
+      extensions: ['js', 'jsx', 'ts', 'tsx'],
+      layout: '_layout',
+    }),
+  ],
   resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
