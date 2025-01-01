241596f9ad0d19ca191dbb9016160150711032fd
feelslikemmmm@gmail.com <67893516+feelslikemmmm@users.noreply.github.com>
2025-01-01 23:16:32 +0900

    styles(low): global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -3,4 +3,10 @@ import reset from 'styled-reset';
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box; /* 기본 박스 모델 설정 */
+  }
+  
 `;
---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const breakpoints = {
+  mobile: '576px',
+  tablet: '768px',
+  laptop: '1024px',
+  desktop: '1200px',
+  wide: '1920px',
+};
+
+export const theme = {
+  breakpoints,
+};
+
+export default theme;
