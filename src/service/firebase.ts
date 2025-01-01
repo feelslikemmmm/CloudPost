a1c81af33c9720f9a7323a0bc55e814d8b28677b
feelslikemmmm@gmail.com <67893516+feelslikemmmm@users.noreply.github.com>
2025-01-01 23:15:57 +0900

    feat(low): firebase setup

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,6 +25,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-export { auth, provider };
+export { auth, googleProvider };
